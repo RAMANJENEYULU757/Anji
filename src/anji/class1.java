b6e2d0a39b4395d563dd87bce9dde8f973b1c2ef
lenovo <lenovo@192.168.1.29>
2025-04-22 16:29:56 +0530

    Create New project

---
+++ src/anji/class1.java
@@ -0,0 +1,12 @@
+package anji;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		System.out.println("Ramanjeneyulu");
+		System.out.println("Ajay");
+		System.out.println("Bhaskar");
+
+	}
+
+}
